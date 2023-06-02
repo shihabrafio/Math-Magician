9221bcc525a61468346b96935563d66775ebbd41
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-02 16:24:53 +0600

    Fetch Data from API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 import Calculator from './component/calculator';
+import FetchData from './component/fetch';
 
 function App() {
-  return (<Calculator />);
+  return (
+    <div>
+      <div><Calculator /></div>
+      <div><FetchData /></div>
+    </div>
+  );
 }
 
 export default App;
