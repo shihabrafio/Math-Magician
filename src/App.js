95640d47cbe308b2dafd594b3c15401e5f32c9ec
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-05 14:09:40 +0600

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Route, Routes } from 'react-router-dom';
 import Calculator from './component/calculator';
 import FetchData from './component/fetch';
+import Header from './component/Header';
+import Home from './component/Home';
 
 function App() {
   return (
-    <div>
-      <div><Calculator /></div>
-      <div><FetchData /></div>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/Quote" element={<FetchData />} />
+        <Route path="/Calculator" element={<Calculator />} />
+        <Route path="/" element={<Home />} />
+
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
