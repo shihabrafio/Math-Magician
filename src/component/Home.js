831ff68c1e282c77248a114ec60da62aa7b1f00e
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-05 14:05:56 +0600

    Add Home Page

---
+++ src/component/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => (
+  <main>
+    <h3>Welcome to my page</h3>
+    <p>
+      Math magicians is a website for all fans of mathematics. It is a Single Page
+      App (SPA) that allows users to:
+      make simple calculations and read a random math-related
+      quote. Enjoy
+    </p>
+  </main>
+);
+
+export default Home;
