4777b4c5c48772a96c4d701d6c6f0845de171885
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-07 17:02:41 +0600

    Add operate testing

---
+++ src/component/operator.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+
+const Operators = ({ cal }) => (
+  <div className="operator">
+    {['÷', 'X', '-', '+', '='].map((op) => (
+      <button type="button" onClick={cal} className="button" key={op}>
+        {op}
+      </button>
+    ))}
+  </div>
+);
+Operators.propTypes = { cal: PropTypes.func.isRequired };
+export default Operators;
